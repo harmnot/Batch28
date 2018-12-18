f34db7a1970070499999ca9ebeaf19fb11d6aa01
Muhammad <myhoomac@hotmail.com>
2018-12-18 16:10:20 +0700

    tugas week2 batch28

---
+++ phase-0/week2/rabu/berlatih-perulangan2.js
@@ -0,0 +1,35 @@
+console.log('\n' + ' ******************--------------*******************' + '\n');
+
+// Berlatih menggunakan While:
+
+var i = 2;
+
+console.log( "LOOPING PERTAMA" );
+while( i <= 20 ) {
+  console.log( i, "I Love Coding" );
+  i+=2;
+};
+
+var j = 22;
+
+console.log( '\n', 'LOOPING KEDUA' );
+while( j-=2) {
+  console.log( j, "I will become fullstack developer" );
+};
+
+console.log('\n' + ' ******************--------------*******************' + '\n');
+
+// berlatih menggunakan FOR:
+
+console.log( 'LOOPING PERTAMA' );
+for( var i = 1 ; i <= 20; i++ ) {
+  console.log( i, "I Love Coding" );
+};
+
+
+console.log( '\n', 'LOOPING KEDUA' );
+for(var j = 20; j >= 1 ; j-- ) {
+  console.log( j, "I will become fullstack developer" );
+};
+
+console.log('\n' + ' ******************--------------*******************' + '\n');
