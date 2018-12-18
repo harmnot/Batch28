d94b0c63334f04a660f337c9ef9667fa2039f156
Muhammad <myhoomac@hotmail.com>
2018-12-18 17:34:35 +0700

    tugas week2 batch28

--- phase-0/week2/rabu/berlatih-perulangan2.js
+++ phase-0/week2/rabu/berlatih-perulangan2.js
@@ -33,3 +33,51 @@ for(var j = 20; j >= 1 ; j-- ) {
 };
 
 console.log('\n' + ' ******************--------------*******************' + '\n');
+
+//  Agka Ganjil dan Genap:
+
+for( var z = 1 ; z <= 100 ; z++ ) {
+  if( z % 2 === 0 ) {
+    console.log( z, 'Angka Genap ini' )
+  } else {
+    console.log( z, '------------------->', 'Angka Ganji' )
+  };
+};
+
+console.log('\n' + ' ******************--------------*******************' + '\n');
+
+var g= 1;
+while( g <= 100 ) {
+  if( g % 3 === 0 ) {
+    console.log( g, 'ini Kelipatan 3' );
+  } else {
+    console.log( ' ' );
+  };
+  g+=2
+};
+
+console.log('\n' + ' ******************--------------*******************' + '\n');
+
+var x = 1;
+while( x <= 100 ) {
+  if( x % 6 === 0 ) {
+    console.log( x, "Kelipatan 6" );
+  } else {
+    console.log( ' ' );
+  };
+  x+=5
+};
+
+console.log('\n' + ' ******************--------------*******************' + '\n');
+
+var k = 1;
+while( k <= 100 ) {
+  if( k % 10 === 0 ) {
+    console.log( k, "kelipatan 10" );
+  } else {
+    console.log( ' ' );
+  };
+  k+=9
+};
+
+console.log('\n' + ' ******************--------------*******************' + '\n');
