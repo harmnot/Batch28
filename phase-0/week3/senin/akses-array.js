e41b697500fd1233fc1e8b861f517a8c2152d137
Muhammad <myhoomac@hotmail.com>
2019-01-08 20:43:36 +0700

    tugas week3 batch28

--- phase-0/week3/senin/akses-array.js
+++
@@ -1,9 +0,0 @@
-function balikString( str ) {
-  var balik = "", arr = str.length;
-  while( arr-- ) {
-    balik += str[ arr ];
-  }
-  return balik;
-};
-
-console.log( balikString("Hello World") );
